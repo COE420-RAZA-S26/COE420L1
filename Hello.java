c275b6f236a89e3f87dbc2af318d2d720c29b1cc
ssharif07 <b00101141@aus.edu>
2026-01-20 12:14:30 +0400

    Feature B added

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        
+        featureB();
+    }
+    
+    public static void featureB() {
+    	System.out.println("I was added");
     }
 }
